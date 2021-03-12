5215edfdf0dceef504dadd19bbbdc35f8c1197cb
Ashfand <ashfand.s@simformsolutions.com>
2021-03-12 18:32:11 +0530

    first commit

---
+++ ChatServerPlayground/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ashfandshaikh/Desktop/MyProjects/Chat_project/ChatServerPlayground/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
